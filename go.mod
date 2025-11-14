9a52ca20fe1301f054d8f891f25e4abc59ca3943
Yulia Medvedeva <zfdokj1111@gmail.com>
2025-11-14 16:08:26 +0300

    исправленые тесты

---
+++ go.mod
@@ -0,0 +1,3 @@
+module polyglot_test
+
+go 1.25.4
--- node/calc.test.js
+++ node/calc.test.js
@@ -2,12 +2,12 @@ const assert = require('assert')
 const { add, sub, mul, div } = require('./calc')
 
 try {
-  assert.strictEqual(add(2,3), 5)
-  assert.strictEqual(sub(5,3), 2)
-  assert.strictEqual(mul(4,5), 20)
-  assert.strictEqual(div(10,2), 5)
+  assert.strictEqual(add(2, 3), 5)
+  assert.strictEqual(sub(5, 3), 2)
+  assert.strictEqual(mul(4, 5), 20)
+  assert.strictEqual(div(10, 2), 5)
   let threw = false
-  try { div(1,0) } catch (e) { threw = true }
+  try { div(1, 0) } catch (e) { threw = true }
   assert.ok(threw, "expected division by zero to throw")
   console.log("All Node.js tests passed")
 } catch (e) {
